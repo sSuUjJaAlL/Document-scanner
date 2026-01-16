a8c06f36f5c8dc882b5a97566b5b8092e4ec0293
LOQ <LOQ@DESKTOP-TSSELJ1>
2026-01-16 15:21:15 +0545

    Added cd

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
 FROM node:20-alpine
 
 WORKDIR /app
-
+    
 COPY package*.json ./
 RUN npm ci
 
 COPY . .
 RUN npm run build --if-present
 
+EXPORT 
 CMD ["npm", "start"]
 
--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,4 +1,5 @@
 import tesseract from "../helper/tesseract.helper"
+import modelResponseMapper from "../mappers/response.mapper"
 import UserRepository from "../repository/user.repositry"
 import givebase from "../utils/base64.utils"
 
@@ -20,7 +21,7 @@ async function uploadfileServiceAndAnalyze(filepath:string){
     const file= await givebase(filepath)
 
     const savetodb = await tesseract(filepath)
-
+    const response = modelResponseMapper(savetodb)
        return{
         data:savetodb,
         message:'SAved'
